1908a611e2d03f76125e2a0ba776ec9229f52ffd
zhengke <zhengke996@163.com>
2024-12-31 10:22:13 +0800

    fix: 后端项目初始化

---
+++ Doj-back/src/test/java/fun/timu/doj/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.timu.doj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
