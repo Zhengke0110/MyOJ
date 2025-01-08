a9baa06e996f1e106dd9b92f65f7fde9fb0cda39
zhengke <zhengke996@163.com>
2025-01-09 00:39:52 +0800

    fix:  前端 password input 样式调整

--- oj-fronted/src/views/Login.vue
+++ oj-fronted/src/views/Login.vue
@@ -35,7 +35,7 @@
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
-              class="w-6 h-6 mx-4 text-gray-400 transition-colors duration-300 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400"
+              class="w-4 h-4 mx-4 text-gray-400 transition-colors duration-300 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400"
             >
               <path d="M12 15a3 3 0 100-6 3 3 0 000 6z" />
               <path
